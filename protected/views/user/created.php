878041707d8e430aa127ccd3206c9199d85619cc
jariel <jariel@jariel-notebook>
2014-05-06 21:07:19 -0300

    Simulando conflito

--- protected/views/user/created.php
+++ protected/views/user/created.php
@@ -3,8 +3,6 @@
 /* @var $dataProvider CActiveDataProvider */
 ?>
 
-<meta http-equiv="Refresh" content="4; url=./index.php?r=site/login">
-
 <h1>Usuário criado com sucesso!</h1>
 
 <h1>Redirecionando para a tela de login...</h1>
